029409f11b6fed8b2020db7cafa6f7495e64c35c
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-18 10:36:11 -0500

    question 3 complete

---
+++ assn-3.js
@@ -0,0 +1,15 @@
+function areYouHere(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    const el1 = arr1[i];
+    for (let j = 0; j < arr2.length; j++) {
+      const el2 = arr2[j];
+      if (el1 === el2) return true;
+    }
+  }
+  return false;
+}
+
+/* Big O =  O(n^2)
+Explaination: The algorithm contains a nested for loop meaning that the
+size of the inputs will scale the number of actions exponentially
+resulting in n^2.*/
