537fdcf244f50f472b56adbeae9c805665095ac9
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-18 11:04:50 -0500

    question 8 complete

---
+++ assn-8.js
@@ -0,0 +1,26 @@
+function efficientSearch(array, item) {
+  let minIndex = 0;
+  let maxIndex = array.length - 1;
+  let currentIndex;
+  let currentElement;
+
+  while (minIndex <= maxIndex) {
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = array[currentIndex];
+
+    if (currentElement < item) {
+      minIndex = currentIndex + 1;
+    }
+    else if (currentElement > item) {
+      maxIndex = currentIndex - 1;
+    }
+    else {
+      return currentIndex;
+    }
+  }
+  return -1;
+}
+
+/* Big O = Logarithmic O(log(n))
+Explanation: This is logn because it reduces the amount of
+actions that need to be taken with each iteration of the while loop. */
